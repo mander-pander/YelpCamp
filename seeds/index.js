57a906c24100a3f88191b8ef42e13deb31a2e706
Amanda Godsey <adgodsey@gmail.com>
2021-05-10 20:01:53 -0700

    added random image, description and price to display

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ullam incidunt consequatur praesentium magnam, explicabo, rem rerum cumque facilis voluptatibus voluptate quos adipisci quas expedita? Magnam esse unde nihil omnis!',
+            price
         })
         await camp.save();
     }
